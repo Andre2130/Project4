62553ccf74f756a5f7f758d0a24e11ae9ae4c640
Andre Robinson <andrerobinson@Andres-MacBook-Pro.local>
2017-11-10 16:18:35 -0500

    added artist_data

---
+++ db/artist_data.rb
@@ -0,0 +1,25 @@
+def get_artist_data
+    artist_data = {
+      'kanyewest' => {
+        name: 'Kanye West',
+        photo_url: 'http://images.deccanchronicle.com/dc-Cover-8kh9nivuhlg71q1gda44u6ku16-20170415225140.Medi.jpeg',
+      },
+      '50cent' => {
+        name: '50 Cent',
+        photo_url: 'http://img.wennermedia.com/featured-promo-782/rs-12-50-cent.jpg',
+      },
+      'Drake' => {
+        name: 'Drake',
+        photo_url: 'http://wethesauce.com/wp-content/uploads/2016/08/Drake-Profile.jpg',
+      },
+      'absoul' => {
+        name: 'Ab-Soul',
+        photo_url: 'http://wethesauce.com/wp-content/uploads/2016/08/ab-soul.jpeg',
+      },
+      'kendricklamar' => {
+        name: 'Kendrick Lamar',
+        photo_url: 'http://cache.umusic.com/_sites/kendricklamar.com/images/og.jpg',
+      }
+    }
+    artist_data
+  end
