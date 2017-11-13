11da2d8f47c96b63ccd5fd069b5ac434c0a57fd0
Andre Robinson <andrerobinson@Andres-MacBook-Pro.local>
2017-11-13 11:35:57 -0500

    added albums controller

---
+++ app/controllers/api/albums_controller.rb
@@ -0,0 +1,37 @@
+class Api::AlbumsController < ApplicationController
+    def index
+        @albums = Album.all
+        render json: @albums
+      end
+    
+      def create
+        @album = Album.create!(album_params)
+    
+        render json: @album
+      end
+    
+      def show
+        @album = Album.find(params[:id])
+    
+        render json: @album
+      end
+    
+      def update
+        @album = Album.find(params[:id])
+        @album.update!(album_params)
+    
+        render json: @album
+      end
+    
+      def destroy
+        @album = Album.find(params[:id]).delete
+    
+        render status: :ok
+      end
+    
+      private
+    
+      def album_params
+        params.require(:album).permit(:name, :photo_url, :nationality)
+      end
+end
