cb7525e12ca2b177cadd295056cd8259ae448d88
Andre Robinson <andrerobinson@Andres-MacBook-Pro.local>
2017-11-14 10:12:00 -0500

    created Artist and artistlist components

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import './App.css';
+import axios from 'axios'
 import NavBar from './components/NavBar'
-
+import ArtistList from './components/ArtistList'
+import Artist from './components/Artist'
 class App extends Component {
+  state = {
+    artists: [],
+    songs: []
+  }
+
+  async componentWillMount () {
+      try {
+        const response = await axios.get('/api/artists')
+        this.setState({artists: response.data})
+      } catch(error){
+          console.log(error)
+      }
+      try {
+        const response = await axios.get('/api/artists_id/songs')
+        this.setState({songs: response.data})
+      } catch(error){
+          console.log(error)
+      }
+  }
   render() {
+    const ArtistListComponent = () => (<ArtistList artists={this.state.artists}/>)
     return (
+      <Router>
       <div className="App">
-       <NavBar />
-        <p className="App-intro">
-          Artist Go here
-        </p>
+          <NavBar />
+          <Switch>
+              <Route exact path='/' render={ArtistListComponent} />
+              <Route exact path='/artists/:artist_id' component={Artist}/>
+          </Switch>
+          
       </div>
+      </Router>
     );
   }
 }
-
 export default App;
